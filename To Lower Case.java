274a8e0bca5d434204a74733c3a522c93d109b4c
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2023-05-07 18:43:02 +0530

    Create To Lower Case.java

---
+++ To Lower Case.java
@@ -0,0 +1,9 @@
+// Qustion: Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+// Solution : 
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
