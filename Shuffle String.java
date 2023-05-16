d61649340a277ad9f2d359626974e4edca9271ed
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2023-05-16 22:01:19 +0530

    Create  Shuffle String.java

---
+++ Shuffle String.java
@@ -0,0 +1,18 @@
+/* You are given a string s and an integer array indices of the same length.
+The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+Return the shuffled string. */
+
+// Solution :-
+
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        StringBuilder str = new StringBuilder("");
+        str.setLength(s.length());
+        for(int i=0;i<indices.length;i++){
+        str.setCharAt(indices[i], s.charAt(i));
+        }
+    return str.toString();
+    }
+}
+
+ 
