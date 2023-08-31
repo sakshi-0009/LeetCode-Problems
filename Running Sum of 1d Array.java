afad7eb42cda63f94fda0164b28464b60ded0d3a
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2023-08-31 10:40:46 +0530

    Running Sum of 1d Array.java

--- Running Sum of 1d Array.java
+++ Running Sum of 1d Array.java
@@ -1,4 +1,4 @@
-/* Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+/* Problem :- Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 Return the running sum of nums.*/
 
 // Solution :-
