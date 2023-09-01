4e2cc9ca9ba6d4a145966cb3520bd553d45cc916
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2023-09-01 23:05:25 +0530

    Create Maximum Achievable Number.dart

---
+++ Maximum Achievable Number.dart
@@ -0,0 +1,13 @@
+/* Problem:- You are given two integers, num and t.
+An integer x is called achievable if it can become equal to num after applying the following operation no more than t times:
+Increase or decrease x by 1, and simultaneously increase or decrease num by 1.
+Return the maximum possible achievable number. It can be proven that there exists at least one achievable number.
+*/
+
+// Solution:-
+
+class Solution {
+  int theMaximumAchievableX(int num, int t) {
+      return num + 2 * t;
+  }
+}
