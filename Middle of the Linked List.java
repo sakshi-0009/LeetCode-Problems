54765daef1cf724dddc361a61596aa78416b79ec
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2023-09-02 14:40:15 +0530

    Middle of the Linked List.java

---
+++ Middle of the Linked List.java
@@ -0,0 +1,17 @@
+// Problem:-Given the head of a singly linked list, return the middle node of the linked list. If there are two middle nodes, return the second middle node.
+
+// Solution :-
+
+public class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
